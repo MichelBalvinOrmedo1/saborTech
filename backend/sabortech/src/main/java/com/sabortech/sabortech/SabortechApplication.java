83d00952605500777eb1954c69f3ccdc887d162c
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-09-22 14:18:30 -0500

    fix .env in aplication

--- backend/sabortech/src/main/java/com/sabortech/sabortech/SabortechApplication.java
+++ backend/sabortech/src/main/java/com/sabortech/sabortech/SabortechApplication.java
@@ -3,20 +3,21 @@ package com.sabortech.sabortech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class SabortechApplication {
 
 	public static void main(String[] args) {
 		// Cargar las variables de entorno desde el archivo .env en la carpeta raíz del
 		// proyecto
-		Dotenv dotenv = Dotenv.configure()
-				.directory(System.getProperty("user.dir"))
-				.load();
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		// Dotenv dotenv = Dotenv.configure()
+		// .directory(System.getProperty("user.dir"))
+		// .load();
+		// System.setProperty("SPRING_DATASOURCE_URL",
+		// dotenv.get("SPRING_DATASOURCE_URL"));
+		// System.setProperty("SPRING_DATASOURCE_USERNAME",
+		// dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		// System.setProperty("SPRING_DATASOURCE_PASSWORD",
+		// dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
 		SpringApplication.run(SabortechApplication.class, args);
 	}
