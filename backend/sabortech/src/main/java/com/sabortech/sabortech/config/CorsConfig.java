9ae8e519b9356d1680a57cb6ea731337a690e40a
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-09-22 15:00:31 -0500

    Feat add Corps dominio

--- backend/sabortech/src/main/java/com/sabortech/sabortech/config/CorsConfig.java
+++ backend/sabortech/src/main/java/com/sabortech/sabortech/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Cambia esto al dominio de tu frontend
+                        .allowedOrigins("https://sabortech.onrender.com") // Cambia esto al dominio de tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
