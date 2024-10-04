64603feb612908c8676b2464d895c395cccdd1a0
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-10-03 20:32:38 -0500

    Fix move to another repository

--- front-end/src/enums/timeFormatRecipe.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum TimeFormatRecipe {
-  MINUTE = "minute",
-  HOUR = "hour",
-  DAY = "day",
-}
--- front-end/src/helpers/recipeFilterCategory.ts
+++
@@ -1,8 +0,0 @@
-import { RecipeListItem } from "../models/Recipes";
-
-export const recipeFilterCategory = (
-  data: RecipeListItem[],
-  categoryName: string
-) => {
-  return data.filter((recipe) => recipe.category.includes(categoryName));
-};
